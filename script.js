3eb65346b14149d7578a9e9a201c68dff170b174
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-03-09 20:58:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var question=prompt("Enter your question")
+
+if(question == '2+2'){
+    alert("4")
+}
+else if(question == '2-1'){
+    alert("3")
+}
+else if(question == '3+3'){
+    alert("6")
+}
+else if(question == '10*2'){
+    alert("20")
+}
+else if(question == '20+20'){
+    alert("40")
+}
+else if(question == '30-5'){
+    alert("25")
+}
